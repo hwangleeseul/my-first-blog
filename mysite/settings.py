12d909ddc13dc6dde0f2983349137346cb331f6a
hwnagleeseul <hwang94621@gmail.com>
2020-01-16 12:06:17 +0900

    sample.html

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$=c9+pc-22z)&t8$3&ft_23kgu0+3q+dhi22+vzabc88i)mxg('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #서비스 단계에서 정적파일을 한곳에 모아둔 곳
 #debug가 true이면 작동하지 않는다.
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #개발단계에서 정적파일들의 위치
 STATICFILES_DIRS = (
